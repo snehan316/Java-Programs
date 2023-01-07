497d1ea231b1fd164eafe8b8bf3b50acc62bd039
Sneha <63293229+snehan316@users.noreply.github.com>
2023-01-08 00:02:09 +0530

    Level 1 problem

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -3,8 +3,23 @@ package com.sneha;
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+      int reversedNum = reverseNum(123);
+      System.out.println(reversedNum);
+	}
+	
+	private static int reverseNum(int num) {
+		
+		int reverse = 0;
+		System.out.println("Input num = " + num);
+		while(num > 0) {
+			int remainder = num % 10;
+			reverse = reverse * 10 + remainder;
+			num = num / 10;
+		}
+		
+		return reverse;
+		
 	}
 
 }
